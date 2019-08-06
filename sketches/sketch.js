daa881b98ae0c65ebdaeda125f9b0c122d0410a5
OVERRUNDERR <OLIVERBEVAN2005@GMAIL.COM>
2019-08-06 10:39:16 +0100

    fix: fix and move dummy data

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,13 +1,48 @@
-function setup() {
-  // create a canvas
-  createCanvas(600, 800);
-}
+const testData = {
+    "companyA": {
 
-function draw() {
-  // colour the background black
-  background(0);
-  // select white as a colour
-  fill(255);
-  // draw a rectangle
-  rect(150, 150, 100, 100);
-}
+      'sharePrice': [67, 70, 76, 56, 45, 40, 32, 25],
+      'profit': [45, 50, 56, 60, 62, 62, 61, 58]
+    },
+
+    "companyB": {
+
+      'sharePrice': [67, 70, 76, 56, 45, 40, 32, 25],
+      'profit': [45, 50, 56, 60, 62, 62, 61, 58]
+    },
+
+    "companyC": {
+      'sharePrice': [67, 70, 76, 56, 45, 40, 32, 25],
+      'profit': [45, 50, 56, 60, 62, 62, 61, 58]
+    },
+
+    "companyD": {
+      'sharePrice': [67, 70, 76, 56, 45, 40, 32, 25],
+      'profit': [45, 50, 56, 60, 62, 62, 61, 58]
+    },
+
+    "ukInterestRate": [0.67, 0.70, 0.76, 0.56, 0.45, 0.40, 0.32, 0.25],
+    "GBPtoUSD": [1.67, 1.70, 1.76, 1.56, 1.45, 1.40, 1.32, 1.25]
+
+    }
+
+
+    console.log(testData)
+
+
+
+
+
+    function setup() {
+      // create a canvas
+      createCanvas(600, 800);
+    }
+
+    function draw() {
+      // colour the background black
+      background(0);
+      // select white as a colour
+      fill(255);
+      // draw a rectangle
+      rect(150, 150, 100, 100);
+    }
